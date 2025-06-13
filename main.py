a43c8af2d475a43c7fb2d9538e5467ff989b02d9
ko022-em <matt.newick@gmail.com>
2025-06-13 16:17:21 +1200

    created screen and infinite game update loop

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame # type: ignore
 from constants import *
 
 def main():
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+
+    #importing the GUI screen mode
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #setting up an infinite loop for game updates
+    num = 0
+    while num == 0:
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+            return
+    pygame.surface.fill(255,255,255)
+    pygame.display.flip()
 
 if __name__ == "__main__":
+   
     main()
+
