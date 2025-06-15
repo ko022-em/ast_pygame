3bb533862f8485966b7315a0dd116f9120433968
ko022-em <matt.newick@gmail.com>
2025-06-15 18:16:38 +1200

    added player, screen and rotation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
 
@@ -11,17 +13,26 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
-
     #importing the GUI screen mode
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #creating a game clock for FPS and tick updates
+    clock = pygame.time.Clock()
+    dt = 0
+    # setting up the player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     #setting up an infinite loop for game updates
-    num = 0
-    while num == 0:
-        for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-            return
-    pygame.surface.fill(255,255,255)
-    pygame.display.flip()
+    while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            screen.fill("black")
+            player.draw(screen)
+            pygame.display.flip()
+
+            # limit the framerate to 60 FPS
+            dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
    
